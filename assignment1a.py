93df1324a899fccf77349e7a1ae6abbcded24ca4
Shambhavi-21 <141634589+Shambhavi-21@users.noreply.github.com>
2023-08-11 10:54:15 +0530

    Add files via upload

---
+++ assignment1a.py
@@ -0,0 +1,8 @@
+#program to find the radius of a circle.
+Radius=float(input("Enter the radius of a circle:"))
+pi=3.8013271108436504
+area=pi*(float(Radius)**2)
+print(area)
+"""Radius=float(input("enter the radius of a circle:"))
+area=3.8013271108436504*(float(Radius)**2)
+print(area)"""
