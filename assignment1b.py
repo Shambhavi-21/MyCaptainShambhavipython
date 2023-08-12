6fe1d8d72f0b51a8614d6e0d6e827b7a96494201
Shambhavi-21 <141634589+Shambhavi-21@users.noreply.github.com>
2023-08-12 08:06:20 +0530

    Delete assignment1b.py

--- assignment1b.py
+++
@@ -1,8 +0,0 @@
-#program to print the extension of a file
-filename=input("enter the name of the file:")
-dots=filename.split(".")
-if(len(dots))>1:
-    extension=dots[-1]
-    print(extension)
-else:
-    print("")
